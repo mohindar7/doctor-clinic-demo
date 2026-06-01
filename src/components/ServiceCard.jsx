440d42593b3afb8afeb18ac461ed40d08908e843
WorkScribe <dev.workscribe@gmail.com>
2026-06-01 22:40:12 +0530

    fix: restore Book now button pill alignment using negative margin on cards

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -10,7 +10,7 @@ export default function ServiceCard({ title, description, iconPath, onBook, clas
       </div>
       <h3>{title}</h3>
       <p className="body-medium">{description}</p>
-      <button className="btn btn-text" style={{ alignSelf: 'flex-start', paddingLeft: 0, marginTop: 'auto' }}>
+      <button className="btn btn-text" style={{ alignSelf: 'flex-start', marginLeft: '-16px', marginTop: 'auto' }}>
         Book now
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round" style={{ marginLeft: '4px' }}>
           <line x1="5" y1="12" x2="19" y2="12"></line>
